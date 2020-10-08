81fd22105ec0c5fa460e6efbc34958cfbeea29f4
Divya gupta <53807822+Divya19gupta@users.noreply.github.com>
2020-10-08 14:26:40 +0530

    Create Array_Del.cpp
    Array Del code

---
+++ Array_Del.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{//hacktober fun
+	int arr[50], size, i, del, count=0;
+	cout<<"Enter array size : ";
+	cin>>size;
+	cout<<"Enter array elements : ";
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+		
+		
+		//hacktober fun
+	}
+	cout<<"Enter element to be delete : ";
+	cin>>del;
+	for(i=0; i<size; i++)
+	{
+		if(arr[i]==del)
+		{//for hacktober fest
+			for(int j=i; j<(size-1); j++)
+			{
+				arr[j]=arr[j+1];
+			}
+			count++;
+			break;
+		}
+	}
+	if(count==0)
+	{
+		cout<<"Element not found..!!";
+	}
+	else
+	{
+		cout<<"Element deleted successfully..!!\n";
+		cout<<"Now the new array is :\n";
+		for(i=0; i<(size-1); i++)
+		{
+			cout<<arr[i]<<" ";
+		}
+	}
+	return 0;
+}
